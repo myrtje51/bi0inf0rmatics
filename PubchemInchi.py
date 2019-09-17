027e59593d947be7f51a244478e567f286e78d43
myrtje51 <55392072+myrtje51@users.noreply.github.com>
2019-09-17 14:42:50 +0200

    Update PubchemInchi.py

--- PubchemInchi.py
+++ PubchemInchi.py
@@ -2,7 +2,6 @@ import sys
 import pubchempy as pcp
 
 def STITCH_inchikey():
-    pubmedID_list = []
     with open('protein_chemical_links_v5.0_2.0.tsv') as f:
         next(f)
         for line in f:
@@ -10,12 +9,9 @@ def STITCH_inchikey():
             pubmedID_unfiltered = fields[0]
             pubmedID = pubmedID_unfiltered[4::]
             c = pcp.Compound.from_cid(pubmedID)
-            print(c) 
-            #pubmedID_list = []
-            #pubmedID_list.append(pudmed_ID)
-            #print(pubmedID_list) 
+            print(c.inchikey)  
 
 def main():
     STITCH_inchikey()
 
-main() 
+main()
