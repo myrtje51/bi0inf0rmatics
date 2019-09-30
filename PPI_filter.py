9b3b47727bc4dc977afbefb1cc1a3183c0f7244c
Myrthe <myrthe_dh@live.nl>
2019-09-30 16:15:09 +0200

    update voor ppi filter

--- PPI_filter.py
+++ PPI_filter.py
@@ -2,7 +2,7 @@ def PPIfilter():
     with open('9606.protein.links.v11.0.txt') as protein_links:
         next(protein_links)
         ppis = open("protein_links_v11.0_2.0.tsv","w+")
-        ppis.write("chemical\tprotein\tcombined_score\n")
+        ppis.write("chemical protein combined_score\n")
         for ppi in protein_links:
             fields_ppi = ppi.strip().split()
             combined_score_ppi = fields_ppi[2]
