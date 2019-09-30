c7c80233d3d3fea7437f0f7f2966ffb464a0dabb
myrtje51 <55392072+myrtje51@users.noreply.github.com>
2019-09-30 15:39:20 +0200

    Add files via upload

---
+++ PPI_filter.py
@@ -0,0 +1,16 @@
+def PPIfilter():
+    with open('9606.protein.links.v11.0.txt') as protein_links:
+        next(protein_links)
+        ppis = open("protein_links_v11.0_2.0.tsv","w+")
+        ppis.write("chemical\tprotein\tcombined_score\n")
+        for ppi in protein_links:
+            fields_ppi = ppi.strip().split()
+            combined_score_ppi = fields_ppi[2]
+            if int(combined_score_ppi) > 699:
+                ppis.write(ppi)
+        ppis.close() 
+
+def main():
+    PPIfilter()
+
+main() 
