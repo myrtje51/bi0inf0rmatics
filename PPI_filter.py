e8c5fd4f92be8583875749d926d3c1296edb1972
Myrthe <myrthe_dh@live.nl>
2019-10-01 11:12:39 +0200

    update object oriented

--- PPI_filter.py
+++ PPI_filter.py
@@ -1,16 +1,18 @@
-def PPIfilter():
-    with open('9606.protein.links.v11.0.txt') as protein_links:
-        next(protein_links)
-        ppis = open("protein_links_v11.0_2.0.tsv","w+")
-        ppis.write("chemical protein combined_score\n")
-        for ppi in protein_links:
-            fields_ppi = ppi.strip().split()
-            combined_score_ppi = fields_ppi[2]
-            if int(combined_score_ppi) > 699:
-                ppis.write(ppi)
-        ppis.close() 
+class PPIfilter:
+    def get_PPIfilter(self):
+        with open('9606.protein.links.v11.0.txt') as protein_links:
+            next(protein_links)
+            ppis = open("protein_links_v11.0_0.9.tsv","w+")
+            ppis.write("chemical protein combined_score\n")
+            for ppi in protein_links:
+                fields_ppi = ppi.strip().split()
+                combined_score_ppi = fields_ppi[2]
+                if int(combined_score_ppi) > 899: 
+                    ppis.write(ppi)
+            ppis.close() 
 
 def main():
-    PPIfilter()
+    filtered = PPIfilter()
+    print(filtered.get_PPIfilter()) 
 
-main() 
+main()
